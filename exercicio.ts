ddbed24acd1dc9582d3441f509568b0a57051706
Douglas Cunha <douglas.reis.cunha@gmail.com>
2023-12-26 16:24:28 -0300

    Final

---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Testando as funções
+console.log(multiplicacao(5, 3)); // Deve imprimir 15
+console.log(saudacao("Douglas"));   // Deve imprimir "Olá Douglas"
